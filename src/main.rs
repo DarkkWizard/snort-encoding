905d4aefcc8bf2849b9fd4e4285368323193c9e5
Grady O'Keefe <gradyokeefe8@gmail.com>
2025-04-13 19:13:03 -0400

    cleaned up the main file, better flow

--- src/main.rs
+++ src/main.rs
@@ -35,41 +35,31 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
 
     match args.action {
-        Action::Encode => match args.mode {
-            Mode::HuffmanSolo => {
-                let text = std::fs::read_to_string(args.input)?;
-                let dest = args.output.clone();
-                eprintln!("found and read file");
+        Action::Encode => {
+            let text = std::fs::read_to_string(args.input)?;
+            let dest = args.output.clone();
+            eprintln!("found and read file");
 
-                let compressed_datums = encode_huffman_solo(
-                    // this is so dumb but I want it for the extra functionality in the other
-                    // length inputs.
-                    &text,
-                    |x| x.chars().map(|g| g.to_string()),
-                    |y| {
-                        y[0].chars().into_iter().fold(
-                            HashMap::new(),
-                            |mut acc: HashMap<_, _>, ch: char| {
-                                *acc.entry(ch.to_string()).or_insert(0) += 1;
-                                acc
-                            },
-                        )
-                    },
-                    Mode::HuffmanSolo,
-                )?;
-                eprintln!("compressed the given data");
-
-                let file = std::fs::File::create(&dest)?;
-                let mut bw = BufWriter::new(&file);
-                let _ = bw.write(&compressed_datums);
-                eprintln!("writen compressed data to {:?}", &args.output);
-            }
-            Mode::HuffmanChunks => {
-                let text = std::fs::read_to_string(args.input)?;
-                let dest = args.output.clone();
-                eprintln!("found and read file");
-
-                let compressed_datums = encode_huffman_solo(
+            let compressed_datums = match args.mode {
+                Mode::HuffmanSolo => {
+                    encode_huffman_solo(
+                        // this is so dumb but I want it for the extra functionality in the other
+                        // length inputs.
+                        &text,
+                        |x| x.chars().map(|g| g.to_string()),
+                        |y| {
+                            y[0].chars().into_iter().fold(
+                                HashMap::new(),
+                                |mut acc: HashMap<_, _>, ch: char| {
+                                    *acc.entry(ch.to_string()).or_insert(0) += 1;
+                                    acc
+                                },
+                            )
+                        },
+                        Mode::HuffmanSolo,
+                    )?
+                }
+                Mode::HuffmanChunks => encode_huffman_solo(
                     &text,
                     |x| {
                         let mut chars = x.chars();
@@ -89,49 +79,35 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                         )
                     },
                     Mode::HuffmanChunks,
-                )?;
-                eprintln!("compressed the given data");
+                )?,
+                Mode::HuffmanCounts => {
+                    todo!();
+                }
+            };
+            eprintln!("compressed the given data");
 
-                let file = std::fs::File::create(&dest)?;
-                let mut bw = BufWriter::new(&file);
-                let _ = bw.write(&compressed_datums);
-                eprintln!("writen compressed data to {:?}", &args.output);
-            }
-            Mode::HuffmanCounts => {
-                todo!();
-            }
-        },
-        Action::Decode => match args.mode {
-            Mode::HuffmanSolo => {
-                let dest = args.output.clone();
-                let raw_data = std::fs::read(args.input)?;
-                eprintln!("Read the input data");
+            let file = std::fs::File::create(&dest)?;
+            let mut bw = BufWriter::new(&file);
+            let _ = bw.write(&compressed_datums);
+            eprintln!("writen compressed data to {:?}", &args.output);
+        }
+        Action::Decode => {
+            let dest = args.output.clone();
+            let raw_data = std::fs::read(args.input)?;
+            eprintln!("Read the input data");
 
-                let uncompressed_data = decode_huffman_solo::<char>(&raw_data)?;
-                eprintln!("Decopmressed data in __ time");
+            let uncompressed_data = match args.mode {
+                Mode::HuffmanSolo => decode_huffman_solo::<char>(&raw_data)?,
+                Mode::HuffmanCounts => decode_huffman_solo::<String>(&raw_data)?,
+                Mode::HuffmanChunks => todo!(),
+            };
+            eprintln!("Decopmressed data in __ time");
 
-                let file = std::fs::File::create(&dest)?;
-                let mut bw = BufWriter::new(&file);
-                let _ = bw.write(&uncompressed_data.into_bytes());
-                eprintln!("Written decompressed data to {:?}", &args.output);
-            }
-            Mode::HuffmanCounts => {
-                todo!();
-            }
-            Mode::HuffmanChunks => {
-                let dest = args.output.clone();
-                let raw_data = std::fs::read(args.input)?;
-                eprintln!("Read the input data");
-
-                let uncompressed_data = decode_huffman_solo::<String>(&raw_data)?;
-                eprintln!("Decopmressed data in __ time");
-
-                let file = std::fs::File::create(&dest)?;
-                let mut bw = BufWriter::new(&file);
-                let _ = bw.write(&uncompressed_data.into_bytes());
-                eprintln!("Written decompressed data to {:?}", &args.output);
-            }
-        },
+            let file = std::fs::File::create(&dest)?;
+            let mut bw = BufWriter::new(&file);
+            let _ = bw.write(&uncompressed_data.into_bytes());
+            eprintln!("Written decompressed data to {:?}", &args.output);
+        }
     }
 
     Ok(())
